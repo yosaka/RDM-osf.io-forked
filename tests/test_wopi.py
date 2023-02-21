328bcf97c6cfafe39a5b1ccfa23f6d5f9318f7a2
yosaka <git@epo.gr.jp>
2023-02-21 13:57:41 +0900

    Fix test code error.

--- tests/test_wopi.py
+++ tests/test_wopi.py
@@ -1,19 +1,23 @@
 import unittest
-from flask import request
-
+import pytest
 import mock
 from nose.tools import *
+
+from flask import request
+from flask import Flask
+
 from osf.models import BaseFileNode
 from website import settings
 from website.wopi.views import check_file_info, _get_user_info, _get_file_info
 
-from flask import Flask
-app = Flask(__name__)
-
+class Filenode():
+    name = ''
 
 class TestCase1(unittest.TestCase):
 
     def setUp(self):
+        self.app = Flask(__name__)
+
         self.user_info = {
             'user_id':   'uid',
             'full_name': 'full name',
@@ -25,40 +29,47 @@ class TestCase1(unittest.TestCase):
             'file_mtime': 2023012345
         }
 
+        self.filenode = Filenode()
+        self.filenode.name = ''
 
-    @mock.patch('util.requests')
-    def test_case1(self, mock_requests):
+    @pytest.mark.django_db
+    def test_case1(self):
 
-        mock_base_file_node = mock.MagicMock()
-        mock_base_file_node.load.return_value = 'filename1.docx'
-        with mock.patch('basefilenode.BaseFileNode', mock_base_file_node):
-            mock_get_user_info = mock.MagicMock()
-            mock_get_user_info.return_value = self.user_info
-            with mock.patch('views._get_user_info', mock_get_user_info):
-                mock_get_file_info = mock.MagicMock()
-                mock_get_file_info.return_value = self.file_info
-                with mock.patch('views._get_file_info', mock_get_file_info):
-                    with app.test_request_context('/wopi', headers={'Cookie': 'osf=Cookie String', 'User_Agent': 'COOLWSD'}, query_string={'access_token': 'Token'}):
+        with self.app.test_request_context('/wopi', headers={'Cookie': 'osf=Cookie String', 'User_Agent': 'COOLWSD'}, query_string={'access_token': 'Token'}):
+            self.filenode.name = 'filename1.docx'
+            settings.WOPI_SRC_HOST = 'http://srchost'
+
+            mock_base_file_node = mock.MagicMock()
+            mock_base_file_node.load.return_value = self.filenode
+            with mock.patch('website.wopi.views.BaseFileNode', mock_base_file_node):
+                mock_get_user_info = mock.MagicMock()
+                mock_get_user_info.return_value = self.user_info
+                with mock.patch('website.wopi.views._get_user_info', mock_get_user_info):
+                    mock_get_file_info = mock.MagicMock()
+                    mock_get_file_info.return_value = self.file_info
+                    with mock.patch('website.wopi.views._get_file_info', mock_get_file_info):
                         res = check_file_info(file_id_ver='ABCDEFG-01')
-                        #print('{}, {}'.format(res['BaseFileName'], res['Size']))
                         assert_equal(res['BaseFileName'], 'filename.docx')
                         assert_equal(res['Size'], 1234)
 
+    @pytest.mark.django_db
+    def test_case2(self):
 
-    @mock.patch('util.requests')
-    def test_case2(self, mock_requests):
+        with self.app.test_request_context('/wopi', headers={'Cookie': 'osf=Cookie String', 'User_Agent': 'Node.js'}, query_string={'access_token': 'Token'}):
+            self.filenode.name = 'filename1.docx'
+            settings.WOPI_SRC_HOST = 'http://srchost'
 
-        mock_base_file_node = mock.MagicMock()
-        mock_base_file_node.load.return_value = 'filename1.docx'
-        with mock.patch('basefilenode.BaseFileNode', mock_base_file_node):
-            mock_get_user_info = mock.MagicMock()
-            mock_get_user_info.return_value = self.user_info
-            with mock.patch('views._get_user_info', mock_get_user_info):
-                mock_get_file_info = mock.MagicMock()
-                mock_get_file_info.return_value = self.file_info
-                with mock.patch('views._get_file_info', mock_get_file_info):
-                    with app.test_request_context('/wopi', headers={'Cookie': 'osf=Cookie String', 'User_Agent': 'Node.js'}, query_string={'access_token': 'Token'}):
+            mock_base_file_node = mock.MagicMock()
+            mock_base_file_node.load.return_value = self.filenode
+            with mock.patch('website.wopi.views.BaseFileNode', mock_base_file_node):
+                mock_get_user_info = mock.MagicMock()
+                mock_get_user_info.return_value = self.user_info
+                with mock.patch('website.wopi.views._get_user_info', mock_get_user_info):
+                    mock_get_file_info = mock.MagicMock()
+                    mock_get_file_info.return_value = self.file_info
+                    with mock.patch('website.wopi.views._get_file_info', mock_get_file_info):
                         res = check_file_info(file_id_ver='ABCDEFG-01')
-                        #print('{}, {}'.format(res['BaseFileName'], res['Size']))
                         assert_equal(res['BaseFileName'], 'filename.docx')
                         assert_equal(res['Size'], 1234)
+                        assert_equal(res['Version'], '01')
+
