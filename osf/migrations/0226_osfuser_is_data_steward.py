2d0b8e1d66f856a15e4767fb7ce4e410d51f8995
huanphan-tma <pmhuan@tma.com.vn>
2023-04-07 14:15:47 +0700

    refs (1)4.1.データ管理責任者ログイン制御: Rename migration file name

--- osf/migrations/0226_osfuser_is_data_steward.py
+++ osf/migrations/0226_osfuser_is_data_steward.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('osf', '0224_ensure_schema_and_reports'),
+        ('osf', '0225_add_pattern_and_norm_to_registration_schema_block'),
     ]
 
     operations = [
