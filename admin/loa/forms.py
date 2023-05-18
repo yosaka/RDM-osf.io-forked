aba0e8d35acb55fa159d2bf0ed44265e9230be2e
Yusaku Kitabatake <y-kitabatake@web-trickster.com>
2023-05-18 11:44:00 +0900

    R-2022-48

--- admin/loa/forms.py
+++ admin/loa/forms.py
@@ -8,8 +8,8 @@ class LoAForm(forms.ModelForm):
 
     CHOICES_AAL = [('', _('NULL')),(1, _('AAL1')),(2, _('AAL2'))]
     CHOICES_IAL = [('', _('NULL')),(1, _('IAL1')),(2, _('IAL2'))]
-    aal = forms.ChoiceField(choices=CHOICES_AAL)
-    ial = forms.ChoiceField(choices=CHOICES_IAL)
+    aal = forms.ChoiceField(choices=CHOICES_AAL,required=False)
+    ial = forms.ChoiceField(choices=CHOICES_IAL,required=False)
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
