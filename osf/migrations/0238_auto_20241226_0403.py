b2b3aa1fb6394e427495e4eca36190ac8e85f7db
Yusaku Kitabatake <y-kitabatake@web-trickster.com>
2024-06-21 17:35:19 +0900

    Add migrations 0238_auto_20241226_0403

--- osf/migrations/0238_auto_20241226_0403.py
+++ osf/migrations/0238_auto_20241226_0403.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('osf', '0237_ensure_schema_and_reports.py'),
+        ('osf', '0237_ensure_schema_and_reports'),
     ]
 
     operations = [
