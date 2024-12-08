f870c8cb4aa9379ac79f35259211aae5844444aa
yosaka <osaka@rsi-inc.co.jp>
2024-12-08 15:17:32 +0900

    travis error correction.

--- addons/onlyoffice/tests/test_token.py
+++ addons/onlyoffice/tests/test_token.py
@@ -20,55 +20,55 @@ class TestOnlyofficeToken(OsfTestCase):
     def test_case1(self):
         # 'auth' is not exist : False
         data = {
-            'data' : {
-                'file_id' : File_id
+            'data': {
+                'file_id': File_id
                 },
-            'exp' : int(datetime.now(timezone.utc).timestamp() +
-                      timedelta(seconds=settings.WOPI_TOKEN_TTL).seconds) +
-                      settings.WOPI_EXPIRATION_TIMER_DELAY
+            'exp': int(datetime.now(timezone.utc).timestamp() +
+                       timedelta(seconds=settings.WOPI_TOKEN_TTL).seconds) +
+                       settings.WOPI_EXPIRATION_TIMER_DELAY
         }
         assert _check_schema(data) == False
 
     def test_case2(self):
         # 'file_id' is not exist : False
         data = {
-            'data' : {
-                'auth' : Cookie
+            'data': {
+                'auth': Cookie
                 },
-            'exp' : int(datetime.now(timezone.utc).timestamp() +
-                        timedelta(seconds=settings.WOPI_TOKEN_TTL).seconds) +
-                        settings.WOPI_EXPIRATION_TIMER_DELAY
+            'exp': int(datetime.now(timezone.utc).timestamp() +
+                       timedelta(seconds=settings.WOPI_TOKEN_TTL).seconds) +
+                       settings.WOPI_EXPIRATION_TIMER_DELAY
         }
         assert _check_schema(data) == False
 
     def test_case3(self):
         #  exp timeouted : False
         data = {
-            'data' : {
-                'auth' : Cookie,
-                'file_id' : File_id
+            'data': {
+                'auth': Cookie,
+                'file_id': File_id
                 },
-            'exp' : int(datetime.now(timezone.utc).timestamp() -
-                      timedelta(seconds=10).seconds)
+            'exp': int(datetime.now(timezone.utc).timestamp() -
+                       timedelta(seconds=10).seconds)
             }
         assert check_token(data, File_id) == False
 
     def test_case4(self):
         # fair token : True
         data = {
-            'data' : {
-                'auth' : Cookie,
-                'file_id' : File_id
+            'data': {
+                'auth': Cookie,
+                'file_id': File_id
                 },
-            'exp' : int(datetime.now(timezone.utc).timestamp() +
-                      timedelta(seconds=settings.WOPI_TOKEN_TTL).seconds) +
-                      settings.WOPI_EXPIRATION_TIMER_DELAY
+            'exp': int(datetime.now(timezone.utc).timestamp() +
+                       timedelta(seconds=settings.WOPI_TOKEN_TTL).seconds) +
+                       settings.WOPI_EXPIRATION_TIMER_DELAY
             }
         assert _check_schema(data) == True
-        
+
     def test_encrypt(self):
         encrypted = encrypt(Cookie, File_id)
         decrypted = decrypt(encrypted)
-        
+
         assert decrypted['data']['auth'] == Cookie
-        assert decrypted['data']['file_id'] ==  File_id
+        assert decrypted['data']['file_id'] == File_id
